24faed938001c3807060b3cb0c82f3f58e67e11b
Milind-dev <milinddev1011@gmail.com>
2026-08-11 22:24:27 +0530

    setup done

--- README.md
+++ README.md
@@ -68,3 +68,49 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+
+
+
+
+
+
+  p tag
+  =========
+    font-size: 18px;
+    max-width: 720px;
+    text-align: center;
+
+
+    .innercontent h2 span {
+    display: block;
+    font-weight: 600;
+
+
+
+        font-family: 'Gotu';
+    src: url(Gotu-Regular.woff2) format('woff2'), url(Gotu-Regular.woff) format('woff');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+}
+
+
+.testimonails h2 {
+    font-size: 22px;
+    text-transform: uppercase;
+    letter-spacing: 4px;
+    margin-bottom: 54px;
+    font-size: 40px;
+    font-weight: 400;
+}
+
+
+.name {
+    font-size: 20px;
+    font-weight: 800;
+    color: #31373d;
+    letter-spacing: 1px;
+    margin-bottom: 8px;
+}
--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
+import "./loader.css";
 
-function App() {
+export default function Loader() {
+  const barRef = useRef(null);
+  const textRef = useRef(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const progress = { value: 1 };
+
+    gsap.to(progress, {
+      value: 100,
+      duration: 15,
+      ease: "power2.out",
+
+      onUpdate: () => {
+        const value = Math.round(progress.value);
+
+        // Update bar width
+        gsap.set(barRef.current, {
+          width: `${value}%`,
+        });
+
+        // Update percentage
+        if (textRef.current) {
+          textRef.current.textContent = `${value}%`;
+        }
+      },
+
+      onComplete: () => {
+        setLoading(false);
+      },
+    });
+  }, []);
+
+  if (!loading) return null;
+
   return (
-    <div className="App">
-      <p> dsfaf </p>
+    <div className="loader">
+      <div className="loader-content">
+        <div className="percentage" ref={textRef}>
+          1%
+        </div>
+
+        <div className="bar-container">
+          <div className="bar" ref={barRef}></div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
